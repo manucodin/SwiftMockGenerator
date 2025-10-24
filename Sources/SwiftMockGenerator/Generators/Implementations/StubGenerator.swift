d0e81f579f43012ce9363abad3034622bb250358
Manu Rodríguez <manurs93@gmail.com>
2025-10-24 10:04:03 -0300

    Refactors generator implementations and models

--- Sources/SwiftMockGenerator/Generators/Implementations/StubGenerator.swift
+++ Sources/SwiftMockGenerator/Generators/Implementations/StubGenerator.swift
@@ -1,13 +1,5 @@
 import Foundation
 
-// MARK: - Mock Generator Protocol
-
-/// Protocol for different types of mock generators
-public protocol MockGeneratorProtocol {
-    func generateMock(for element: CodeElement, annotation: MockAnnotation, useResult: Bool) throws -> String
-    func generateMockDefinition(for element: CodeElement, annotation: MockAnnotation, useResult: Bool) throws -> String
-}
-
 // MARK: - Stub Generator
 
 /// Generates stub implementations with default return values
